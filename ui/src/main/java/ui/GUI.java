5c2dcc34a0770552f3debce127015913c94d5df3
Joseph Diza <josephm.diza@gmail.com>
2025-03-28 15:28:05 -0700

    Jot notes later for GUI

--- ui/src/main/java/ui/GUI.java
+++ ui/src/main/java/ui/GUI.java
@@ -8,6 +8,23 @@ import javafx.stage.Stage;
 
 // User Interface
 
+// Core:
+// 
+// Show a gallery with plant picture thumbnails & plant names/prices 
+// - User clicks on plant, navigates to a plant panel which shows
+//   - Bigger picture of the plant
+//   - Bold Title Name
+//   - Bold Title Price ($CAD)
+//   - Further information about the plant below
+// - Every panel has navigation buttons:
+//   - Forward/Backward
+
+// Admin Panels to modify plant price/information
+//
+// Extra:
+// 
+// About Dialog Window which shows credits & information
+
 // We're going to have a graphical user interface that is simmilar
 // to online e-commerce stores, with a picture of the plant, and
 // its details listed.
