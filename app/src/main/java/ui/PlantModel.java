a812d67ef534d5dee68d646e002f283059e5b10e
Joseph Diza <josephm.diza@gmail.com>
2025-04-05 22:25:03 -0700

    Create initial Gallery

---
+++ app/src/main/java/ui/PlantModel.java
@@ -0,0 +1,15 @@
+package ui;
+
+import plant.AbstractPlant;
+
+// import javafx.beans.property.DoubleProperty;
+// import javafx.beans.property.SimpleDoubleProperty;
+
+public class PlantModel {
+  // Properties
+  // private final DoubleProperty price = new SimpleDoubleProperty();
+  // private final price = new SimpleDoubleProperty();
+
+  // Data
+  private AbstractPlant plant;
+}
---
+++ app/src/main/java/ui/PlantView.java
@@ -0,0 +1,64 @@
+package ui;
+
+import javafx.geometry.Orientation;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PlantView {
+  // Widgets
+  private Label lbl_plant_name;
+  private Label lbl_plant_price;
+  private Button btn_plant_view;
+  private Separator sep_plant ;
+  private ImageView iv; 
+
+  private HBox hb1;
+  private HBox hb2;
+  private VBox vb;
+  
+  // MVC
+  private PlantController controller;
+  private PlantModel model;
+
+  public PlantView(final PlantController controller, final PlantModel model) {
+    this.controller = controller;
+    this.model = model;
+
+    // TODO: Configure panels, layout, controls, listeners
+    
+    this.lbl_plant_name = new Label("Plant Name");
+    this.lbl_plant_price = new Label("Plant Price");
+    this.btn_plant_view = new Button("View");
+    this.sep_plant = new Separator(Orientation.HORIZONTAL);
+    this.iv = new ImageView("file:/home/jmd/Media/Pictures/Avatars/default-user.png");
+
+    this.hb1 = new HBox();
+    this.hb2 = new HBox();
+    this.vb = new VBox();
+    
+    // TODO: Configure properties
+    vb.setPrefWidth(100);
+    vb.setPrefHeight(200);
+
+    // Packing
+    hb1.getChildren().addAll(lbl_plant_name);
+    hb2.getChildren().addAll(lbl_plant_price, sep_plant, btn_plant_view);
+    vb.getChildren().addAll(
+        iv,
+        hb1,
+        hb2
+    );
+  }
+
+  // Internal
+
+  // API
+  public Parent asParent() {
+    return this.vb;
+  }
+}
