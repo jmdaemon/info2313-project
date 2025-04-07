6227abca4cfcaba1056da4a09656afd58c5e1410
Joseph Diza <josephm.diza@gmail.com>
2025-04-07 16:21:37 -0700

    Create Component, Resource Navigator interfaces

---
+++ app/src/main/java/ui/Navigator.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public interface Navigator {
+  /**
+   * Set elements of a component to navigate to new scenes
+   **/
+   abstract void setNavigateEvent(final String elem, Scene root, Parent next);
+}
